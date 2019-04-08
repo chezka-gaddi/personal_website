3aca3e0d3ff1227cfdc1b8b81ce83653c5dda8e4
Gaddi <7180120@linux09.mcs.sdsmt.edu>
2019-04-08 15:44:19 -0600

    Removed unused files

--- studentRes/index.php
+++ studentRes/index.php
@@ -8,13 +8,12 @@ error_reporting(E_ALL);
 <html>
 
 <head>
+    <title>Home</title>
     <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet">
-    <script src="js/script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="styles/header.css">
     <link rel="stylesheet" type="text/css" href="styles/layout.css">
 </head>
-<title>Home</title>
 
 <body>
     <?php include 'php/header.php'; ?>
--- studentRes/js/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var curr_page = document.title.toLocaleLowerCase();
-    console.log(curr_page);
-    var nav = document.getElementById(curr_page);
-    nav.classList.add("active");
-}
